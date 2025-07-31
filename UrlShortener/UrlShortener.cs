c931517fc3435624c9c2cbcc6370b677f4e0504f
Péter Gyöngyösi <gyongyosi.peter@marktsoft.hu>
2025-07-31 16:17:22 +0200

    Add UrlShortener class; add getter functions

---
+++ UrlShortener/UrlShortener.cs
@@ -0,0 +1,44 @@
+﻿namespace UrlShortener
+{
+    public class UrlShortener
+    {
+        private readonly IUrlMapDb _urlMapDb;
+
+        public UrlShortener(IUrlMapDb urlMapDb)
+        {
+            _urlMapDb = urlMapDb;
+        }
+
+        public string GetShortUrlByLong(string longUrl)
+        {
+            if (string.IsNullOrWhiteSpace(longUrl))
+            {
+                throw new ArgumentException("Url cannot be empty");
+            }
+
+            var shortUrl = _urlMapDb.GetShortUrl(longUrl);
+            if (shortUrl == null)
+            {
+                throw new KeyNotFoundException("Long url not found");
+            }
+
+            return shortUrl;
+        }
+
+        public string GetLongUrlByShort(string shortUrl)
+        {
+            if (string.IsNullOrWhiteSpace(shortUrl))
+            {
+                throw new ArgumentException("Url cannot be empty");
+            }
+
+            var longUrl = _urlMapDb.GetLongUrl(shortUrl);
+            if (longUrl == null)
+            {
+                throw new KeyNotFoundException("Short url not found");
+            }
+
+            return longUrl;
+        }
+    }
+}
