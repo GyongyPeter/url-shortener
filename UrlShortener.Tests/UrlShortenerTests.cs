a402b3818003eb619a9cf907a7b07f845f60e1d8
Péter Gyöngyösi <gyongyosi.peter@marktsoft.hu>
2025-07-31 16:11:32 +0200

    Add some tests

--- UrlShortener.Tests/UrlShortenerTests.cs
+++ UrlShortener.Tests/UrlShortenerTests.cs
@@ -2,6 +2,41 @@ namespace UrlShortener.Tests
 {
     public class UrlShortenerTests
     {
-        
+        private readonly UrlShortener _urlShortener;
+        private readonly InMemoryUrlMapDb _urlMapDb;
+
+        public UrlShortenerTests()
+        {
+            _urlMapDb = new InMemoryUrlMapDb();
+            _urlShortener = new UrlShortener(_urlMapDb);
+        }
+
+        [Fact]
+        public void GetShortUrlByNormal_ShouldReturnShortUrl()
+        {
+            // Arrange
+            var normalUrl = "https://abacusmedicinegroup.com/";
+            var generatedShortUrl = _urlShortener.SaveUrlMappingToCache(normalUrl);
+
+            // Act
+            var receivedShortUrl = _urlShortener.GetShortUrlByNormal(normalUrl);
+
+            // Assert
+            Assert.Equal(generatedShortUrl, receivedShortUrl);
+        }
+
+        [Fact]
+        public void GetNormalUrlByShort_ShouldReturnOriginalUrl()
+        {
+            // Arrange
+            var normalUrl = "https://abacusmedicinegroup.com/";
+            var generatedShortUrl = _urlShortener.SaveUrlMappingToCache(normalUrl);
+
+            // Act
+            var receivedNormalUrl = _urlShortener.GetNormalUrlByShort(generatedShortUrl);
+
+            // Assert
+            Assert.Equal(normalUrl, receivedNormalUrl);
+        }
     }
 }
