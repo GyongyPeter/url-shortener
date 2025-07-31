88adcc486a8f589a8506edc66ba99ff0e7bfe988
Péter Gyöngyösi <gyongyosi.peter@marktsoft.hu>
2025-07-31 16:07:02 +0200

    Extend database class

--- UrlShortener/IUrlMapDb.cs
+++ UrlShortener/IUrlMapDb.cs
@@ -3,6 +3,7 @@
     public interface IUrlMapDb
     {
         string GetLongUrl(string shortUrl);
+        string GetShortUrl(string longUrl);
         void SaveUrlMapping(string shortUrl, string longUrl);
     }
 }
--- UrlShortener/InMemoryUrlMapDb.cs
+++ UrlShortener/InMemoryUrlMapDb.cs
@@ -3,6 +3,7 @@
     public class InMemoryUrlMapDb : IUrlMapDb
     {
         private readonly Dictionary<string, string> _shortToLong = new();
+        private readonly Dictionary<string, string> _longToShort = new();
 
         public string GetLongUrl(string shortUrl)
         {
@@ -10,9 +11,16 @@
             return longUrl;
         }
 
+        public string GetShortUrl(string longUrl)
+        {
+            _longToShort.TryGetValue(longUrl, out var shortUrl);
+            return shortUrl;
+        }
+
         public void SaveUrlMapping(string shortUrl, string longUrl)
         {
             _shortToLong[shortUrl] = longUrl;
+            _longToShort[longUrl] = shortUrl;
         }
     }
 }
